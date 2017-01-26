d89e174bd8edbf86a15a6ce7afd3deaf1731db85
Nathan <nathanstarwalt@gmail.com>
2017-01-26 14:44:50 -0500

    Second-partial and Third Question

--- main.js
+++ main.js
@@ -13,6 +13,11 @@
 // console.log(itemPriceCurrency);
 
 //Total number of items - used for later calculations
+
+/*
+ * Question #1 - Average Item Price
+ */
+
 var totalNumberOfItems = 0;
 items.forEach(function(eachItem){
   return totalNumberOfItems += 1;
@@ -29,13 +34,37 @@ var itemCurrencyConverter = items.map(function(eachItem, itemId, allItems){
   }
 });
 
-console.log(itemCurrencyConverter);
+// console.log(itemCurrencyConverter);
 //takes the previous array and
 var totalItemPrice = itemCurrencyConverter.reduce(function(first, second){
   return first + second;
 });
-console.log(totalItemPrice);
+// console.log(totalItemPrice);
+
+console.log((totalItemPrice/totalNumberOfItems).toFixed(2));
+
 /*
- * Question #1 - Average Item Price
+ *Question 2
  */
-console.log(totalItemPrice/totalNumberOfItems);
+//Since the format is an array, the map function is being used.
+var priceFinderArray = items.map(function(eachItem, itemId, allItems){
+//The restrictions are being checked here
+  if (eachItem.price > 14 && eachItem.price < 18){
+    //Item title returned here
+    return eachItem.title;
+  }
+});
+
+console.log(priceFinderArray);
+
+/*
+ * Question 3
+ */
+// forEach iterator to check all items...
+items.forEach(function(eachItem, itemId, allItems){
+  //... for GBP
+  if (eachItem.currency_code === "GBP"){
+    //logs item name and price
+    console.log(eachItem.title + " costs £" + eachItem.price);
+  }
+});
